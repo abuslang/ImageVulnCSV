ce64677ab846ce477e4033d28509202dc0650963
Abdus Quadri <asquadri1@gmail.com>
2025-12-09 17:51:49 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+api_key = "api-key"
+api_secret = "api-secret"
+
+# depends on what your url is, app2, app3, etc
+# replace with your url
+url = "https://api2.prismacloud.io/"
+
+
+# Compute Console Address 
+# (e.g. us-east1.cloud.twistlock.com/us-2-123456789)
+# You can include or exclude 'https://', the script handles it.
+compute_url = "https://us-east1.cloud.twistlock.com/us-x-xxxxxxxx"
